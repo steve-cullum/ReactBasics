31c6016ae7ea20a164ecc1fcfb291c88f5673caf
Steve Cullum <steve.cullum@gmail.com>
2018-04-15 18:05:46 +0100

    fix eslint issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-var HTMLWebpackPlugin=require('html-webpack-plugin')
-var HTMLWebpackPluginConfig=new HTMLWebpackPlugin({
-                                            template: __dirname + '/www/index.html',
-                                            filename: 'index.html',
-                                            inject: 'body'
-                                            })
-module.exports = { entry: __dirname + '/www/index.js',
-                    module:{rules: [
-                                        { test: /\.js$/, 
-                                          exclude: /node_modules/,
-                                          loader: 'babel-loader',
-                                          query: {
-                                            presets: ['es2015', 'react']
-                                        }
-                                        }
-                                    ]
-                                },
-                                output: {
-                                    filename: 'transformed.js',
-                                    path: __dirname + '/build'
-                                },
-                                plugins: [HTMLWebpackPluginConfig]
-                             };
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: `${__dirname}/www/index.html`,
+  filename: 'index.html',
+  inject: 'body',
+});
+export const entry = `${__dirname  }/www/index.js`;
+export const module = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react'],
+      },
+    },
+  ],
+};
+export const output = {
+  filename: 'transformed.js',
+  path: `${__dirname}/build`,
+};
+export const plugins = [HTMLWebpackPluginConfig];
