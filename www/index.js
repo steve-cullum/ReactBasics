4267807e783017c726ec669ae2d9a2ab56e0381d
Steve Cullum <steve.cullum@gmail.com>
2018-04-16 20:29:00 +0100

    move to new main folder

--- www/index.js
+++ www/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import App from '../components/main/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
